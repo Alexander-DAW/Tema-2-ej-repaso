011249b05af6982b451a28731bdba73e93a9dab8
Alexander Mora <alexandermoradev@gmail.com>
2025-10-14 21:30:15 +0200

    Ej 48

---
+++ ej44.py
@@ -0,0 +1,9 @@
+tupla=(10, 20, 30)
+a,b,c=tupla
+print(a)
+print(b)
+print(c)
+a, c, b=tupla
+print(a)
+print(b)
+print(c)
---
+++ ej47.py
@@ -0,0 +1,5 @@
+set_n1={1,2,3,4,5}
+set_n2={1,3,6,7,4}
+set_def=(set_n1 | set_n2)
+set_def=list(set_def)
+print(set_def)
