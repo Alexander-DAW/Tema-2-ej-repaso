b5328d7a19809da6e0a012b8848f8435867d2779
AlexanderMora SS <alexandermoradev@gmail.com>
2025-10-31 13:15:19 +0100

    Velada mas avanzada pre examen

--- ejercicios_variables/Velada.py
+++ ejercicios_variables/Velada.py
@@ -1,6 +1,9 @@
 class FueraDeRango(Exception):
     pass
 
+class TituloDuplicado(Exception):
+    pass
+
 class PersonaDuplicada(Exception):
     def __init__(self, nombre_persona):
         super().__init__(f"{nombre_persona} ya aparece en nuestra base de datos. Prueba otra persona. ")
@@ -8,18 +11,18 @@ class PersonaDuplicada(Exception):
 
 class Evento:
     
-    entradas_vendidas = 0
-    
-    def __init__(self, evento):
-        self.__evento = evento
-        self.tipos_entrada = {"general": 25.0,
+    tipos_entrada = {"general": 25.0,
                          "premium": 60.0,
                          "vip": 120.0
                          }
+
+    def __init__(self, evento):
+        self.__evento = {}
         self.__combates = {}
-        self.__artistas = {}
+        self.__artistas = []
         self.__entradas = {}
         self.__staff = {}
+        self.entradas_vendidas = 0
 
     @property
     def evento(self):
@@ -62,13 +65,11 @@ class Evento:
         self.__staff = staff
 
     def montar_reservas(self):
-        self.combastes=[self.combates]
-        self.artistas=[self.artistas]
-        self.entradas=[self.entradas]
-        self.staff=[self.staff]
+        self.evento["combates"] = self.combates
+        self.evento["artistas"] = self.artistas
+        self.evento["entradas"] = self.entradas
 
-
-
+        
     def vender_entradas(self, tipo, unidades):
         costo_entrada = 0
         for nombre_tipo, precio in self.tipos_entrada.items():
@@ -79,10 +80,31 @@ class Evento:
         print(self.entradas)
         print(f"Tipo de entrada: {tipo}\nUnidades: {unidades}\n- Se han vendido {unidades} entradas {tipo} por un total de {costo_entrada} euros")
 
-    def agregar_artista(self, nombre, setlist):
-        print(f"Nombre del artista: {nombre}\n ")
+    def agregar_artista(self, nombre, *setlist):
+        print(f"Nombre del artista: {nombre}\n. Artista añadido correctamente")
+        artista_dic = {
+            "nombre" : nombre,
+            "setlist" : list(setlist)
+        }
+        self.artistas.append(artista_dic)
+        print(self.artistas)
 
+    def programar_combate(self, titulo, luchadores, rounds, peso):
+        datos_combate = {
+            "luchadores" : luchadores,
+            "rounds": rounds,
+            "peso": peso,
+            "resultado" : None
+        }
+        self.combates[titulo] = datos_combate
 
+    def informe_completo(self):
+        self.montar_reservas()
+        print ("Informe de la velada")
+        for i in self.evento:
+            print(f"Numero de combates: {len(combates)}")
+
+        
     
 
 
@@ -99,21 +121,13 @@ class Persona:
         self.__nombre = nombre
 
 class Luchador(Persona):
-    def __init__(self, nombre, peso):
+    def __init__(self, nombre):
         super().__init__(nombre)
-        self.__peso = peso
     
-    @property
-    def peso(self):
-        return self.__peso
-
-    @peso.setter
-    def peso(self, peso):
-        self.__peso = peso
         
 
 class Artista(Persona):
-    def __init__(self, nombre, *temas):
+    def __init__(self, nombre, temas):
         super().__init__(nombre)
         self.__temas = temas
     
@@ -127,8 +141,44 @@ class Artista(Persona):
 
 
 class Combate:
-    def __init__(self, titulo, peso, rounds):
-        pass
+    def __init__(self, titulo, peso, rounds, luchadores):
+        self.__titulo = titulo
+        self.__peso = peso
+        self.__rounds = rounds
+        self.__luchadores = luchadores
+    
+    @property
+    def titulo(self):
+        return self.__titulo
+
+    @titulo.setter
+    def titulo(self, titulo):
+        self.titulo = titulo
+
+    @property
+    def peso(self):
+        return self.__peso
+
+    @peso.setter
+    def peso(self, peso):
+        self.peso = peso
+
+    @property
+    def rounds(self):
+        return self.__rounds
+
+    @rounds.setter
+    def rounds(self, rounds):
+        self.rounds = rounds
+
+    @property
+    def luchadores(self):
+        return self.__luchadores
+
+    @luchadores.setter
+    def luchadores(self, luchadores):
+        self.luchadores = luchadores
+
 
 class Entrada:
 
@@ -155,13 +205,13 @@ class Entrada:
     
 
 
-Velada = Evento({})
+Velada = Evento()
 
-def Menu():
-    print(f"Bienvenido al Backstage Manager de la Velada de Ibai.\nElige una de las siguientes funciones:\n{'-' * 30}\n1. Vender entradas\n2. Añadir artista\n3. Programar combates\n4. Cerrar combate\n5. Ver informe completo del evento\n6. Salir")
+def menu():
+    print(f"Bienvenido al Backstage Manager de la Velada de Ibai.\nElige una de las siguientes funciones:\n{'-' * 30}")
     while True:
         try:
-            user = int(input("Selecciona una opción: "))
+            user = int(input("1. Vender entradas\n2. Añadir artista\n3. Programar combates\n4. Cerrar combate\n5. Ver informe completo del evento\n6. Salir\nSelecciona una opción del menu: "))
             if 1 <= user <= 6:
                 print(f"Opcion {user} elegida.")       
                 match user:
@@ -195,12 +245,53 @@ def Menu():
                                 setlist_artista = [cancion.strip() for cancion in temas_artista.split(",")]
                                 set_setlist = set(setlist_artista)
                                 artista = Artista(nombre_artista, set_setlist)
-                                Velada.agre
+                                if artista.nombre in Velada.artistas:
+                                    raise PersonaDuplicada(nombre_artista)
+                                else:
+                                    Velada.agregar_artista(artista.nombre, artista.temas)
+                                    break
                             except ValueError:
                                 print("Respuesta invalida. Pruebe de nuevo")
+                            except PersonaDuplicada as epd:
+                                print (epd)
+                    case 3:
+                        while True:
+                            try:
+                                lista_luchadores = []
+                                titulo_combate = str(input("Introduza el titulo de combate: ")).strip().lower().capitalize()
+                                rounds = int(input("Introduce el numero de rondas: "))
+                                peso = str(input("Introduce el peso la pelea ")).strip().lower()
+                                try:
+                                    if titulo_combate not in Velada.combates.keys():
+                                        for luchador in range(2):
+                                            try:
+                                                luchador_nombre= str(input(f"Introduce el nombre del luchador {luchador + 1} : ")).strip().lower().capitalize()
+                                                if luchador_nombre not in lista_luchadores:
+                                                    luchador = Luchador(luchador_nombre)
+                                                    lista_luchadores.append(luchador.nombre)
+                                                else:
+                                                    raise PersonaDuplicada (luchador_nombre)
+                                            except ValueError:
+                                                print("Respuesta invalida. Pruebe de nuevo")
+                                            except PersonaDuplicada as e_u:
+                                                print(e_u)
+                                        combate = Combate(titulo_combate, lista_luchadores, rounds, peso )
+                                        Velada.programar_combate(combate.titulo, combate.luchadores, combate.rounds, combate.peso)
+                                    else:
+                                        raise TituloDuplicado("Este titulo ya existe.")                    
+                                    break
+                                except TituloDuplicado as aeo:
+                                    print(aeo)
+                            except ValueError:
+                                print("Respuesta invalida. Pruebe de nuevo")                                    
 
-                            
+                                
+                                    
+                                        
 
+                                        
+
+        
             else:
                 raise FueraDeRango("Opción fuera de rango. Elige un número entre 1 y 6.")
         except ValueError:
@@ -209,4 +300,5 @@ def Menu():
             print(e)
 
 
-Menu()
+if __name__ == "__main__":    
+    menu()
